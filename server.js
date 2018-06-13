8a5dca5fa7117549d15f82005e1374413d5a4fb9
yacin <elmaachy@gmail.com>
2018-06-14 00:30:38 +0200

    port changing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 var app = express();
-
+const port = process.env.PORT || 3000; 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
@@ -50,6 +50,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port`);
 });
